7d52cfd1fa2564c425b8b709fc22a49e0439949e
mjkim0918 <80248003+mjkim0918@users.noreply.github.com>
2021-07-14 16:47:44 +0900

    Update roc_test.R

--- roc_test.R
+++ roc_test.R
@@ -5,8 +5,12 @@ roc_test <- function(y,score1, score2){
   roc1 <- roc(y, score1) 
   roc2 <- roc(y, score2) 
   roc.test(roc1,roc2)
-  
-  result <- round(c(roc.test(roc1,roc2)$estimate,roc.test(roc1,roc2)$p.value), 4) 
+  auc_ci1 <- round(ci.auc(roc1),3)
+  auc_res1 <- paste0(auc_ci1[2],"(",auc_ci1[1],", ",auc_ci1[3],")")
+  auc_ci2 <- round(ci.auc(roc2),3)
+  auc_res2 <- paste0(auc_ci2[2],"(",auc_ci2[1],", ",auc_ci2[3],")")
+  #result <- round(c(roc.test(roc1,roc2)$estimate,roc.test(roc1,roc2)$p.value), 4) 
+  result <- c(auc_res1, auc_res2, round(roc.test(roc1,roc2)$p.value,3))
   names(result) <- c("AUC of roc1", "AUC of roc2", "p_value")
   return(result)
 }
