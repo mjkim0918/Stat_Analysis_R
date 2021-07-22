e0c50eca03872d64e0199ca2eaf70812d1cdf001
mjkim0918 <80248003+mjkim0918@users.noreply.github.com>
2021-07-22 21:07:56 +0900

    Update SNSP_mcnemar .R

--- SNSP_mcnemar .R
+++ SNSP_mcnemar .R
@@ -3,12 +3,20 @@ library("e1071");library(epiR);library(pROC);library(caret);
 library(geepack)
 
 SNSP_mcnemar <- function(truth, pred1, pred2){
+  #Sensitivity & Specificity for pred1
   T1_snsp <- SNSP(truth, pred1)
+  
+  #Sensitivity & Specificity for pred2
   T2_snsp <- SNSP(truth, pred2)
+  
+  #Mcnemar's test for Sensitivity
   data_sn <- as.data.frame(cbind(truth,pred1, pred2)) %>% filter(truth == 1)
-  p_sn <- round(mcnemar.test(data_sn$pred1,data_sn$pred2)$p.value,3)
+  p_sn <- round(mcnemar.test(data_sn$pred1,data_sn$pred2)$p.value, 3)
+  
+  #Mcnemar's test for Specificity
   data_sp <- as.data.frame(cbind(truth,pred1, pred2)) %>% filter(truth == 0)
-  p_sp <- round(mcnemar.test(data_sp$pred1,data_sp$pred2)$p.value,3)
+  p_sp <- round(mcnemar.test(data_sp$pred1,data_sp$pred2)$p.value, 3)
+  
   result <- list(T1_snsp = T1_snsp, T2_snsp = T2_snsp, p_sn = p_sn, p_sp = p_sp)
   return(result)
 } 
