3ad712963becdfbf7819551a9faba2b047d04c16
mjkim0918 <80248003+mjkim0918@users.noreply.github.com>
2021-07-22 19:24:45 +0900

    Update roc_single.R

--- roc_single.R
+++ roc_single.R
@@ -5,6 +5,7 @@ roc_single <- function(truth, score){
   roc <- roc(truth, score,plot=T)
   
   auc_ci <- round(ci.auc(roc),3)
-  auc_res <- paste0(auc_ci[2],"(",auc_ci[1],", ",auc_ci[3],")")
+  auc_ci_res <- paste0(auc_ci[2],"(",auc_ci[1],", ",auc_ci[3],")")
+  auc_res <- list(auc_ci = auc_ci_res, auc = auc_ci[2], auc_LB = auc_ci[1], auc_UB = auc_ci[3])
   return(auc_res)
 }
