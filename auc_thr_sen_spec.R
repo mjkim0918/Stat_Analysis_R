e399136cc868df458b2aab9d38737442a27d4e87
mjkim0918 <80248003+mjkim0918@users.noreply.github.com>
2022-07-05 20:25:34 +0900

    Update and rename auc_thr_sen.R to auc_thr_sen_spec.R

--- auc_thr_sen_spec.R
+++ auc_thr_sen_spec.R
@@ -30,3 +30,36 @@ auc_thr_sen <- function(y, pred, sen){
                  n_positivie =n_positivie, n_true_positive = n_true_positive, Total_n_cancer = Total_n_cancer)
   return(result)
 }
+
+
+
+auc_thr_spec <- function(y, pred, spec){
+  roc <- roc(y,pred,plot=T)
+  
+  auc_ci <- round(ci.auc(roc),3)
+  auc_res <- paste0(auc_ci[2],"(",auc_ci[1],", ",auc_ci[3],")")
+  
+  threshold_spec <- coords(roc) %>% 
+    mutate(diff = specificity - spec) %>% 
+    slice(which.min(abs(diff))) %>% 
+    slice(which.max(sensitivity)) %>% 
+    dplyr::select(-diff)
+  
+  table<-confusionMatrix(ref=factor(y),data=factor(ifelse(pred >= threshold_spec$threshold, 1, 0)),positive="1")
+  n_positivie <- rowSums(table$table)[2] #cut off 적용결과 positivie 로 분류된 사람들
+  n_true_positive <- table$table[2,2] # True positive
+  Total_n_cancer <- colSums(table$table)[2]
+  
+  tb<-table$tab[2:1,2:1]
+  sensitivity_spec <- round(epi.tests(tb)$elements$sensitivity,3)
+  specificity_spec <- round(epi.tests(tb)$elements$specificity,3)
+  threshold_spec <- threshold_spec %>%
+    mutate(sensitivity = paste0(sensitivity_spec[1],"(",sensitivity_spec[2],", ",sensitivity_spec[3],")"),
+           specificity = paste0(specificity_spec[1],"(",specificity_spec[2],", ",specificity_spec[3],")"),
+           threshold = round(threshold, 3))
+  
+  
+  result <- list(auc = auc_res, threshold_spec = threshold_spec, 
+                 n_positivie =n_positivie, n_true_positive = n_true_positive, Total_n_cancer = Total_n_cancer)
+  return(result)
+}
